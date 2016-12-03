474b534e9fbcddaa58b1c7076bf484e3c9f0e8e9
Akaru <akaruakaridesu@gmail.com>
2016-12-03 17:05:05 +0900

    nothing
    testing php

---
+++ test.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>hey</title>
+    </head>
+    <body>
+        <?php
+            $test = $_GET["arg1"]
+            if($test == "one")
+            {
+                echo("<h1>test</h1>");
+            }else if($test == "two")
+            {
+                echo("<h1>hey</h1>");
+            }else if($test == "three")
+            {
+                echo ("<h1>retarded</h1>")
+            }
+        ?>
+    </body>
+</html>
